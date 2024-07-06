0d1a694f4bfe3c3f2fd59440899b809fc60081ff
Jon Nordby <jononor@gmail.com>
2024-07-06 17:15:39 +0200

    TEMP: reduce, still reproduces

--- examples/natmod/distance/distance.c
+++ examples/natmod/distance/distance.c
@@ -1,42 +1,7 @@
 // distance.c
 #include "py/dynruntime.h"
 
-#include <string.h>
 #include <stdint.h>
-
-#if 1
-#define debug_printf(...) mp_printf(&mp_plat_print, "distance-" __VA_ARGS__)
-#else
-#define debug_printf(...) //(0)
-#endif
-
-// Find which vector in @vectors that @v is closes to
-uint16_t
-compute_euclidean3_argmin_uint8(const uint8_t *vectors, int vectors_length,
-            const uint8_t *vv, int channels, uint32_t *out_dist)
-{
-
-    uint16_t min_idx = 0;
-    uint32_t min_value = UINT32_MAX-10;
-    for (int i=0; i<vectors_length; i++) {
-        uint32_t dist = 0;
-
-        for (int j=0; j<channels; j++) {
-            uint8_t c = vectors[(i*channels)+j];
-            uint8_t v = vv[j];
-            dist += (v - c) * (v - c);
-        }
-        if (dist < min_value) {
-            min_value = dist;
-            min_idx = i;
-        }
-    }
-    if (out_dist) {
-        *out_dist = min_value;
-    }
-
-    return min_idx;
-}
 
 // MicroPython API
 static mp_obj_t
@@ -65,23 +30,9 @@ euclidean_argmin(mp_obj_t vectors_obj, mp_obj_t point_obj) {
 
     const int vector_length = values_length / n_channels;
 
-    debug_printf("in values=%d channels=%d vector=%d \n", \
-        (int)values_length, (int)n_channels, (int)vector_length
-    );
-
-#if 1
+    // hardcoded return values
     uint32_t min_dist = vector_length;
     const uint16_t min_index = 1;
-#else
-    
-    uint32_t min_dist = 0;
-    const uint16_t min_index = \
-        compute_euclidean3_argmin_uint8(values, vector_length, point, n_channels, &min_dist);
-
-    debug_printf("out idx=%d dist=%d \n", \
-        (int)min_index, (int)min_dist
-    );
-#endif
 
     return mp_obj_new_tuple(2, ((mp_obj_t []) {
         mp_obj_new_int(min_index),
