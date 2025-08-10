ffa48722e77c641e7186bb81bbcbd5a1e9a0a8df
Jon Nordby <jononor@gmail.com>
2025-08-10 15:38:03 +0200

    xiao: Include emlearn in manifest

--- ports/zephyr/boards/xiao_ble_nrf52840_sense/manifest.py
+++ ports/zephyr/boards/xiao_ble_nrf52840_sense/manifest.py
@@ -4,3 +4,7 @@ require("upysh")
 require("aioble")
 require("neopixel")
 require("aiorepl")
+
+base = './../../emlearn-micropython/src/'
+module("emlearn_trees.py", base_path=base+'emlearn_trees')
+module("emlearn_fft.py", base_path=base+'emlearn_fft')
