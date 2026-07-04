1c9e3c6db3603400f35b681a2c7c7a3c8dadfe9e
Jon Nordby <jononor@gmail.com>
2026-07-04 14:14:05 +0200

    ports/unix: Let Python module run neccesary make steps

--- ports/unix/setup.py
+++ ports/unix/setup.py
@@ -1,8 +1,9 @@
 
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
+from setuptools.command.build_ext import build_ext as build_ext_original
 import sys
 import setuptools
+import subprocess
 
 class get_pybind_include:
     """Helper class to determine the pybind11 include path"""
@@ -10,6 +11,20 @@ class get_pybind_include:
     def __str__(self):
         import pybind11
         return pybind11.get_include()
+
+
+class build_ext(build_ext_original):
+    """
+    Run neccesary make steps before Python module build
+
+    This enables to use pip install for the entire build flow,
+    such as when doing building from a git repo. Example:
+    pip install "git+https://github.com/micropython/micropython.git@main#subdirectory=ports/unix"
+    """
+    def run(self):
+        subprocess.check_call(["make", "submodules"], cwd=".")
+        subprocess.check_call(["make", "clean", "libmicropython", "CFLAGS_EXTRA='-fPIC'"], cwd=".")
+        super().run()
 
 ext_modules = [
     Extension(
